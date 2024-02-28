74a00e599d5bffd5dc4dad15bd1c3e29e423c706
OXGN <cabarrubias.nevinharold@gmail.com>
2024-02-29 01:10:37 +0800

    styles; small change

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Tasks</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
   </head>
   <body>
 
- <div class="container-fluid mt-4">
- <section class="section">
+  <div class="container-fluid mt-4">
+  <section class="section">
       <div class="row">
         <div class="col-lg-12">
 
@@ -25,6 +25,7 @@
 
               <!-- Start Show Table -->
                 <table class="table datatable">
+
                   <!-- Start Showing Table Head -->
                     <thead>
                       <tr>
@@ -32,6 +33,7 @@
                         <th class="col">Description</th>
                         <th class="col">Priority</th>
                         <th class="col">Due Date</th>
+                        <th class="col">Actions</th>
                       </tr>
                     </thead>
                   <!-- End Showing Table Head -->
@@ -47,7 +49,7 @@
                               foreach($query_run as $row)
                             {
                         ?>
-                      <!-- Start Initializing Tasks and Compress it.-->
+                      <!-- End Initializing Tasks and Compress it.-->
                       
                       <!-- Start Showing Compressed file by extracting rows per row in table tasks  -->
                           <tr>
@@ -63,27 +65,31 @@
                             <td>
                               <?= $row['dueDate']; ?>
                             </td>
-                            <td>
-                              
-                              <!-- Start Importing ID to view.php & update.php -->
-                                <a type="button" class="btn btn-outline-primary" href="view.php?id=<?=$row['id'];?>">
-                                  VIEW
-                                </a>
-                                <a type="button" class="btn btn-outline-warning" href="update.php?id=<?=$row['id'];?>" id="updateButton">
-                                  UPDATE
-                                </a>
-                              <!-- End Importing ID to view.php & update.php -->
 
-                              <!-- Start passing ID to process.php in handleDeleteTask -->
-                                <form action="process.php" method="POST">
-                                  <input type="hidden" name="id" value="<?= $row['id']; ?>"> <!-- the id will be pass to process.php-->
-                                  <button type="submit" class="btn btn-outline-danger" name="handleDeleteTask">
-                                    DELETE
-                                  </button>
-                                </form>
-                              <!-- End passing ID to process.php in handleDeleteTask -->
+                            <!-- Start Show Action -->
+                              <td class="action-button">
+                                
+                                <!-- Start Importing ID to view.php & update.php if pressed -->
+                                  <a type="button" class="btn btn-outline-primary spacing" href="view.php?id=<?=$row['id'];?>">
+                                    VIEW
+                                  </a>
+                                  <a type="button" class="btn btn-outline-warning spacing" href="update.php?id=<?=$row['id'];?>" id="updateButton">
+                                    UPDATE
+                                  </a>
+                                <!-- End Importing ID to view.php & update.php if pressed -->
 
-                            </td>
+                                <!-- Start passing ID to process.php in handleDeleteTask -->
+                                  <form action="process.php" method="POST">
+                                    <input type="hidden" name="id" value="<?= $row['id'];?>"> <!-- the id will be pass to process.php-->
+                                    <button type="submit" class="btn btn-outline-danger spacing" name="handleDeleteTask">
+                                      DELETE
+                                    </button>
+                                  </form>
+                                <!-- End passing ID to process.php in handleDeleteTask -->
+
+                              </td>
+                            <!-- End Show Action -->
+
                           </tr>
                       <!-- End Showing Compressed file by extracting rows per row in table tasks  -->
 
